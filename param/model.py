a6f70b450169110bee7a23da5ad32ba1a1638938
Stevin Sony <stevinsony24@gmail.com>
2024-04-01 21:17:12 +0530

    New model stuff

--- param/model.py
+++ param/model.py
@@ -1,122 +1,62 @@
-# import the necessary packages
-from . import config
-from torch.nn import ConvTranspose2d
-from torch.nn import Conv2d
-from torch.nn import MaxPool2d
-from torch.nn import Module
-from torch.nn import ModuleList
-from torch.nn import ReLU
-from torchvision.transforms import CenterCrop
-from torch.nn import functional as F
-import torch
+from tensorflow.keras.layers import (  # type:ignore
+    Conv2D,
+    BatchNormalization,
+    Activation,
+    MaxPool2D,
+    Conv2DTranspose,
+    Concatenate,
+    Input,
+)
+from tensorflow.keras.models import Model  # type:ignore
 
 
-class Block(Module):
-    def __init__(self, inChannels, outChannels):
-        super().__init__()
-        # store the convolution and RELU layers
-        self.conv1 = Conv2d(inChannels, outChannels, 3)
-        self.relu = ReLU()
-        self.conv2 = Conv2d(outChannels, outChannels, 3)
+def conv_block(input, num_filters):
+    x = Conv2D(num_filters, 3, padding="same")(input)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
 
-    def forward(self, x):
-        # apply CONV => RELU => CONV block to the inputs and return it
-        return self.conv2(self.relu(self.conv1(x)))
+    x = Conv2D(num_filters, 3, padding="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
 
-
-class Encoder(Module):
-    def __init__(self, channels=(3, 16, 32, 64)):
-        super().__init__()
-        # store the encoder blocks and maxpooling layer
-        self.encBlocks = ModuleList(
-            [Block(channels[i], channels[i + 1]) for i in range(len(channels) - 1)]
-        )
-        self.pool = MaxPool2d(2)
-
-    def forward(self, x):
-        # initialize an empty list to store the intermediate outputs
-        blockOutputs = []
-        # loop through the encoder blocks
-        for block in self.encBlocks:
-            # pass the inputs through the current encoder block, store
-            # the outputs, and then apply maxpooling on the output
-            x = block(x)
-            blockOutputs.append(x)
-            x = self.pool(x)
-        # return the list containing the intermediate outputs
-        return blockOutputs
+    return x
 
 
-class Decoder(Module):
-    def __init__(self, channels=(64, 32, 16)):
-        super().__init__()
-        # initialize the number of channels, upsampler blocks, and
-        # decoder blocks
-        self.channels = channels
-        self.upconvs = ModuleList(
-            [
-                ConvTranspose2d(channels[i], channels[i + 1], 2, 2)
-                for i in range(len(channels) - 1)
-            ]
-        )
-        self.dec_blocks = ModuleList(
-            [Block(channels[i], channels[i + 1]) for i in range(len(channels) - 1)]
-        )
-
-    def forward(self, x, encFeatures):
-        # loop through the number of channels
-        for i in range(len(self.channels) - 1):
-            # pass the inputs through the upsampler blocks
-            x = self.upconvs[i](x)
-            # crop the current features from the encoder blocks,
-            # concatenate them with the current upsampled features,
-            # and pass the concatenated output through the current
-            # decoder block
-            encFeat = self.crop(encFeatures[i], x)
-            x = torch.cat([x, encFeat], dim=1)
-            x = self.dec_blocks[i](x)
-        # return the final decoder output
-        return x
-
-    def crop(self, encFeatures, x):
-        # grab the dimensions of the inputs, and crop the encoder
-        # features to match the dimensions
-        (_, _, H, W) = x.shape
-        encFeatures = CenterCrop([H, W])(encFeatures)
-        # return the cropped features
-        return encFeatures
+def encoder_block(input, num_filters):
+    x = conv_block(input, num_filters)
+    p = MaxPool2D((2, 2))(x)
+    return x, p
 
 
-class UNet(Module):
-    def __init__(
-        self,
-        encChannels=(3, 16, 32, 64),
-        decChannels=(64, 32, 16),
-        nbClasses=1,
-        retainDim=True,
-        outSize=(config.INPUT_IMAGE_HEIGHT, config.INPUT_IMAGE_WIDTH),
-    ):
-        super().__init__()
-        # initialize the encoder and decoder
-        self.encoder = Encoder(encChannels)
-        self.decoder = Decoder(decChannels)
-        # initialize the regression head and store the class variables
-        self.head = Conv2d(decChannels[-1], nbClasses, 1)
-        self.retainDim = retainDim
-        self.outSize = outSize
+def decoder_block(input, skip_features, num_filters):
+    x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(input)
+    x = Concatenate()([x, skip_features])
+    x = conv_block(x, num_filters)
+    return x
 
-    def forward(self, x):
-        # grab the features from the encoder
-        encFeatures = self.encoder(x)
-        # pass the encoder features through decoder making sure that
-        # their dimensions are suited for concatenation
-        decFeatures = self.decoder(encFeatures[::-1][0], encFeatures[::-1][1:])
-        # pass the decoder features through the regression head to
-        # obtain the segmentation mask
-        map = self.head(decFeatures)
-        # check to see if we are retaining the original output
-        # dimensions and if so, then resize the output to match them
-        if self.retainDim:
-            map = F.interpolate(map, self.outSize)
-        # return the segmentation map
-        return map
+
+def build_unet(input_shape):
+    inputs = Input(input_shape)
+
+    s1, p1 = encoder_block(inputs, 64)
+    s2, p2 = encoder_block(p1, 128)
+    s3, p3 = encoder_block(p2, 256)
+    s4, p4 = encoder_block(p3, 512)
+
+    b1 = conv_block(p4, 1024)
+
+    d1 = decoder_block(b1, s4, 512)
+    d2 = decoder_block(d1, s3, 256)
+    d3 = decoder_block(d2, s2, 128)
+    d4 = decoder_block(d3, s1, 64)
+
+    outputs = Conv2D(1, 1, padding="same", activation="sigmoid")(d4)
+
+    model = Model(inputs, outputs, name="U-Net")
+    return model
+
+
+if __name__ == "__main__":
+    input_shape = (512, 512, 3)
+    model = build_unet(input_shape)
+    model.summary()
--- train.py
+++ train.py
@@ -1,42 +1,132 @@
-# USAGE
-# python train.py
-# import the necessary packages
-from param.dataset import SegmentationDataset
-from param.model import UNet
-from param import config
-from torch.nn import BCEWithLogitsLoss
-from torch.optim import Adam
-from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
-from torchvision import transforms
-from imutils import paths
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-import torch
-import time
 import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import numpy as np
+import cv2
+from glob import glob
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.callbacks import (  # type:ignore
+    ModelCheckpoint,
+    CSVLogger,
+    ReduceLROnPlateau,
+)  # type:ignore
+from tensorflow.keras.optimizers import Adam  # type:ignore
+from tensorflow.keras.metrics import Recall, Precision  # type:ignore
+from param.model import build_unet
+
+# from metrics import dice_loss, dice_coef, iou
+
+# """ Global parameters """
+# H = 512
+# W = 512
 
 
-imagePaths = []
-maskPaths = []
-for path in os.listdir(config.IMAGE_DATASET_PATH):
-    imagePaths.append(path)
-for path in os.listdir(config.MASK_DATASET_PATH):
-    maskPaths.append(path)
+# def create_dir(path):
+#     """Create a directory."""
+#     if not os.path.exists(path):
+#         os.makedirs(path)
 
 
-split = train_test_split(
-    imagePaths, maskPaths, test_size=config.TEST_SPLIT, random_state=42
-)
-# unpack the data split
-(trainImages, testImages) = split[:2]
-(trainMasks, testMasks) = split[2:]
+# def load_data(path, split=0.1):
+#     images = sorted(glob(os.path.join(path, "images", "*.png")))
+#     masks = sorted(glob(os.path.join(path, "masks", "*.png")))
 
-for i in range(0, 50):
-    print(trainImages[i], ":", trainMasks[i])
+#     split_size = int(len(images) * split)
 
-print("[INFO] saving testing image paths...")
-f = open(config.TEST_PATHS, "w")
-f.write("\n".join(testImages))
-f.close()
+#     train_x, valid_x = train_test_split(images, test_size=split_size, random_state=42)
+#     train_y, valid_y = train_test_split(masks, test_size=split_size, random_state=42)
+
+#     train_x, test_x = train_test_split(train_x, test_size=split_size, random_state=42)
+#     train_y, test_y = train_test_split(train_y, test_size=split_size, random_state=42)
+
+#     return (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
+
+
+# def read_image(path):
+#     x = cv2.imread(path, cv2.IMREAD_COLOR)
+#     x = cv2.resize(x, (W, H))
+#     x = x / 255.0
+#     x = x.astype(np.float32)
+#     return x
+
+
+# def read_mask(path):
+#     x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+#     x = cv2.resize(x, (W, H))
+#     x = x / np.max(x)
+#     x = x > 0.5
+#     x = x.astype(np.float32)
+#     x = np.expand_dims(x, axis=-1)
+#     return x
+
+
+# def tf_parse(x, y):
+#     def _parse(x, y):
+#         x = x.decode()
+#         y = y.decode()
+
+#         x = read_image(x)
+#         y = read_mask(y)
+#         return x, y
+
+#     x, y = tf.numpy_function(_parse, [x, y], [tf.float32, tf.float32])  # type:ignore
+#     x.set_shape([H, W, 3])
+#     y.set_shape([H, W, 1])
+#     return x, y
+
+
+# def tf_dataset(X, Y, batch=8):
+#     dataset = tf.data.Dataset.from_tensor_slices((X, Y))
+#     dataset = dataset.shuffle(buffer_size=200)
+#     dataset = dataset.map(tf_parse)
+#     dataset = dataset.batch(batch)
+#     dataset = dataset.prefetch(4)
+#     return dataset
+
+
+# if __name__ == "__main__":
+#     """Seeding"""
+#     np.random.seed(42)
+#     tf.random.set_seed(42)
+
+#     """ Directory for storing files """
+#     create_dir("files")
+
+#     """ Hyperparameters """
+#     batch_size = 2
+#     lr = 1e-5
+#     num_epochs = 10
+#     model_path = os.path.join("files", "model.h5")
+#     csv_path = os.path.join("files", "data.csv")
+
+#     """ Dataset """
+#     dataset_path = "UNET/COVID"
+#     (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data(dataset_path)
+
+#     print(f"Train: {len(train_x)} - {len(train_y)}")
+#     print(f"Valid: {len(valid_x)} - {len(valid_y)}")
+#     print(f"Test: {len(test_x)} - {len(test_y)}")
+
+#     train_dataset = tf_dataset(train_x, train_y, batch=batch_size)
+#     valid_dataset = tf_dataset(valid_x, valid_y, batch=batch_size)
+
+#     """ Model """
+#     model = build_unet((H, W, 3))
+#     # metrics = [dice_coef, iou, Recall(), Precision()]
+#     # model.compile(loss=dice_loss, optimizer=Adam(lr), metrics=metrics)
+
+#     callbacks = [
+#         ModelCheckpoint(model_path, verbose=1, save_best_only=True),
+#         ReduceLROnPlateau(
+#             monitor="val_loss", factor=0.1, patience=5, min_lr=1e-7, verbose=1
+#         ),
+#         CSVLogger(csv_path),
+#     ]
+
+#     model.fit(
+#         train_dataset,
+#         epochs=num_epochs,
+#         validation_data=valid_dataset,
+#         callbacks=callbacks,
+#     )
