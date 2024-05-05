e93baeaa0598f7c7bc4a1ecb0c35e94b3dee16cc
Stevin Sony <stevinsony24@gmail.com>
2024-05-05 14:10:55 +0530

    trained model

--- train.py
+++ train.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
-from tensorflow import keras
+from tensorflow import keras  # type:ignore
 
-new_unet = tf.keras.models.load_model("ultrasound_segmentation/output/unet_model.keras")
+new_unet = tf.keras.models.load_model(  # type:ignore
+    "ultrasound_segmentation/output/unet_model.keras"
+)
 print(new_unet.summary())
+loss, acc = new_unet.evaluate()
